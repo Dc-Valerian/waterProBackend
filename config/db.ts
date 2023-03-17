21a715c6fc735c65424fefaf2cf93e9f063153f2
Dc-Valerian <valerianpedro03@gmail.com>
2023-03-17 07:34:30 +0100

    update

--- config/db.ts
+++ config/db.ts
@@ -1,5 +1,7 @@
 import mongoose from "mongoose"
-const URI = "mongodb+srv://ValerianPedro:ValerianPedro@cluster0.ngujblg.mongodb.net/?retryWrites=true&w=majority"
+const URI = "mongodb://127.0.0.1/WaterProject"
+// const URI = "mongodb+srv://valerianpedro:valerianpedro@cluster0.ngujblg.mongodb.net/?retryWrites=true&w=majority"
+
 mongoose.connect(URI)
 mongoose.connection.on("open",()=>{
     console.log("db is on")
--- dist/config/db.js
+++ dist/config/db.js
@@ -4,7 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
-const URI = "mongodb://127.0.0.1/WaterProject";
+const URI = "mongodb+srv://ValerianPedro:ValerianPedro@cluster0.ngujblg.mongodb.net/?retryWrites=true&w=majority";
 mongoose_1.default.connect(URI);
 mongoose_1.default.connection.on("open", () => {
     console.log("db is on");
